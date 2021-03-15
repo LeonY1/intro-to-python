15e24186dd1ffbd96d858d4e25df269832204838
Leon Yuan <leonyyuan@gmail.com>
2021-03-14 21:04:50 -0500

    Starting project

---
+++ main.py
@@ -0,0 +1,36 @@
+# Variable w/ Value
+
+# Variable w/ Variable
+
+# Data Types
+# int
+# float
+# str w/ "" or ''
+# bool
+# casting
+
+# Operators
+# Adding
+# Subtracting
+# Multiplying
+# Dividing
+# Modulus
+# Floor Division
+
+# Boolean Operator
+# ==, >=, >, !=, <=, <
+# and or not
+
+# Lists
+# Matrices
+# Slciing
+# Dictionaries
+
+# Control Structures
+# if...elif...else statements
+# while statements
+# for statements
+
+# Functions (args); Why?
+
+# Importing
